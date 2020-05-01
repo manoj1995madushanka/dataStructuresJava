27b5e33ba4208d46d1d7aa8c6f451fab29f31ec9
manoj1995madushanka <madushankamanoj414@gmail.com>
2020-05-01 21:16:31 +0530

    done

--- src/main/java/List/BasicLinkedList.java
+++ src/main/java/List/BasicLinkedList.java
@@ -10,36 +10,38 @@ public class BasicLinkedList<X> {
 
     public BasicLinkedList() {
         first = null;
-        last= null;
+        last = null;
         nodeCount = 0;
     }
 
-    public int size(){
-        return nodeCount;
-    }
-
-    public void add(X item){
-        if(first==null){
+    public void add(X item) {
+        //this condition means we are adding something for the first time.
+        if(first == null) {
             first = new Node(item);
-            last=first;
-        }else{
+            last = first;
+        }
+        //otherwise, we want to grab the last node and update it's value
+        else {
             Node newLastNode = new Node(item);
             last.setNextNode(newLastNode);
+            last = newLastNode;
         }
         nodeCount++;
     }
 
-    public void insert(X item,int position){
-        if(size()<position){
-            throw new IllegalStateException("the linked list is smaller than the posision you accessed");
+    public void insert(X item, int position) {
+        if(size() < position) {
+            throw new IllegalStateException("The LinkedList is smaller than the position you are trying to insert at: " + position);
         }
 
         Node currentNode = first;
-        //start from 1 bcz we already on the irst node
-        for(int i=1;i<position && currentNode!=null;i++){
+
+        //start at 1 because we are already on the first node
+        for(int i = 1; i < position && currentNode != null; i++) {
             currentNode = currentNode.getNextNode();
         }
-        //connect new node
+
+        //severs the link chain and reconnects with the new node
         Node newNode = new Node(item);
         Node nextNode = currentNode.getNextNode();
         currentNode.setNextNode(newNode);
@@ -47,17 +49,99 @@ public class BasicLinkedList<X> {
         nodeCount++;
     }
 
-    public X remove(){
-        if(first==null){
-            throw new IllegalStateException("The linked list is empty");
+    public X removeAt(int position) {
+        if(first == null) {
+            throw new IllegalStateException("The LinkedList is empty and there are no items to remove");
         }
+
+        Node currentNode = first;
+        Node prevNode = first;
+
+        //start at 1 because we are already on the first node
+        for(int i = 1; i < position && currentNode != null; i++) {
+            prevNode = currentNode;
+            currentNode = currentNode.getNextNode();
+        }
+
+        //now update the pointers and throw away the old first
+        X nodeItem = currentNode.getNodeItem();
+        prevNode.setNextNode(currentNode.getNextNode());
+        nodeCount--;
+        return nodeItem;
+
+    }
+
+    public X remove() {
+        if(first == null) {
+            throw new IllegalStateException("The LinkedList is empty and there are no items to remove");
+        }
+
         X nodeItem = first.getNodeItem();
+
+        //now update the first pointer and throw away the old first
         first = first.getNextNode();
         nodeCount--;
         return nodeItem;
     }
 
-    private class Node{
+    public X get(int position) {
+        if(first == null) {
+            throw new IllegalStateException("The LinkedList is empty and there are no items to get");
+        }
+
+        Node currentNode = first;
+        for(int i = 1; i < size() && currentNode != null; i++) {
+            if(i == position) {
+                return currentNode.getNodeItem();
+            }
+
+            currentNode = currentNode.getNextNode();
+        }
+
+        //if we didn't find it then return null
+        return null;
+    }
+
+    public int find(X item) {
+        if(first == null) {
+            throw new IllegalStateException("The LinkedList is empty and there are no items to find");
+        }
+
+        Node currentNode = first;
+        for(int i = 1; i < size() && currentNode != null; i++) {
+            if(currentNode.getNodeItem().equals(item)) {
+                return i;
+            }
+
+            currentNode = currentNode.getNextNode();
+        }
+
+        //if we didn't find it, then return -1
+        return -1;
+    }
+
+    //useful for pretty print
+    public String toString() {
+        StringBuffer contents = new StringBuffer();
+        Node curNode = first;
+
+        while(curNode != null) {
+            contents.append(curNode.getNodeItem());
+            curNode = curNode.getNextNode();
+
+            if(curNode != null) {
+                contents.append(", ");
+            }
+        }
+        return contents.toString();
+    }
+
+
+    public int size(){
+        return nodeCount;
+    }
+
+    private class Node {
         private Node nextNode;
         private X nodeItem;
 
@@ -66,16 +150,18 @@ public class BasicLinkedList<X> {
             this.nodeItem = item;
         }
 
-        public Node getNextNode() {
-            return nextNode;
-        }
-
         public void setNextNode(Node nextNode) {
             this.nextNode = nextNode;
         }
 
-        public X getNodeItem(){
+        public Node getNextNode() {
+            return nextNode;
+        }
+
+        public X getNodeItem() {
             return nodeItem;
         }
+
     }
+
 }
